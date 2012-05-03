a7c4fce2a95a82c403f66fcb1faa4db3c253f4d8
Eric Lowe <elowe@ucdavis.edu>
2012-05-02 19:38:53 -0700

    Removed Bioperl

--- tutorial_data.pl
+++ tutorial_data.pl
@@ -5,26 +5,20 @@
 # because of better quality.
 
 use warnings; use strict;
-use Bio::SeqIO; use Bio::SeqIO::fasta; use Bio::SeqIO::fastq;
 
 my $input_file = shift;         # Get input file from command line
 #print "File: $input_file\n";
 $input_file =~ m/\w+.(\w+)/;    # Get file format (stored in $1)
 #print "Format: $1\n";
 my $format = $1;                # Store file format in more portable variable
-my $file_size = -s $input_file;
-#print "File size: $file_size\n";
+my $file_size = (-s $input_file) / (1024 * 1024);
+
+print "File size: $file_size\n";
+
 my $lines = `wc -l $input_file`;
 $lines =~ s/\s+\w+.\w+//;
 print "Number of lines: $lines\n";
-my $seq_in = Bio::SeqIO->new(
-                  -format=> $format,
-                  -file => $input_file,
-                  );            # Bioperl object to retrieve next seq in file
 
-while (my $seq = $seq_in->next_seq() ) 
-{
-    next;
-}
-
+my $front_cut = 0.05 * ($lines / 4);
+print "Cut from front: $front_cut\n";
 exit;
