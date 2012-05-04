c1eb667291c08a6927b3c976fdfb1a608b009fc2
Eric Lowe <elowe@ucdavis.edu>
2012-05-03 22:19:11 -0700

    Added comments

--- tutorial_data.pl
+++ tutorial_data.pl
@@ -1,16 +1,20 @@
-#! /usr/bin/perl 
+#! /usr/bin/perl
+# tutorial_data.pl 
 # This is a perl script that takes an input file of seq data
 # and produces a ~20Mb tutorial dataset for Phylosift.
 # Ignores beginning and end of file and uses middle data
 # because of better quality.
-
+# Usage: perl tutorial_data.pl [input file] > [output file]
 use warnings; use strict;
 
-my $input = shift;         # Get input file from command line
+# Get input file from command line
+my $input = shift;        
+# Open bzipped file as decompressed stream into filehandle $fh for reading
+open my $fh, "bzcat '$input' |" or die "Couldn't open [bzcat '$input']:$!";  
 
-open my $fh, "bzcat '$input' |" or die "Couldn't open [bzcat '$input']:$!";
-my $i = 0;
+my $i = 0; # Counting variable
 while (my $line = <$fh>) {
-    print $line if (int($i++ / 4)) % 10 == 0;
+    # Print line if remainder of $i / 4 equals 0
+    print $line if (int($i++ / 4)) % 100 == 0;
 }
 exit;
