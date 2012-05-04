ef308dc47bf725909c66399c4fe6a91c4be4f34a
Eric Lowe <elowe@ucdavis.edu>
2012-05-03 20:43:45 -0700

    Does this work?

--- tutorial_data.pl
+++ tutorial_data.pl
@@ -6,11 +6,11 @@
 
 use warnings; use strict;
 
-my $input_file = shift;         # Get input file from command line
+my $input = shift;         # Get input file from command line
 
-open (IN, "bzcat $input_file") or die "What are you trying to do?";
+open my $fh, "bzcat '$input' |" or die "Couldn't open [bzcat '$input']:$!";
 my $i = 0;
-while (my $line = <IN>) {
+while (my $line = <$fh>) {
     print $line if (int($i++ / 4)) % 10 == 0;
 }
 exit;
