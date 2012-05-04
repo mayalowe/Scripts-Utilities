3ae6fa7f450d396eb1fa0c8d937c8f3bb7e9c367
Eric Lowe <elowe@ucdavis.edu>
2012-05-03 22:02:42 -0700

    Made ($i++/4) % 100

--- tutorial_data.pl
+++ tutorial_data.pl
@@ -11,6 +11,6 @@ my $input = shift;         # Get input file from command line
 open my $fh, "bzcat '$input' |" or die "Couldn't open [bzcat '$input']:$!";
 my $i = 0;
 while (my $line = <$fh>) {
-    print $line if (int($i++ / 4)) % 10 == 0;
+    print $line if (int($i++ / 4)) % 100 == 0;
 }
 exit;
