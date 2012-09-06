75c7390dac749d63da4974fc3a1a7df36b750b19
Eric Lowe <elowe@ucdavis.edu>
2012-09-06 11:22:15 -0700

    testbench not fully operational, but written as it should be

--- megan_to_ps.pl
+++ megan_to_ps.pl
@@ -13,9 +13,10 @@ open my $fh, $input or die "Couldn't open file: $!";
 while(<$fh>)
 {
     chomp(my $line = $_);
+    $line =~ s/-[23]/1/g; # change negative numbers (not assigned/low complexity) to 1 (assign to root)
     $line =~ s/ //g; # remove spaces
     $line =~ s/,/\t/g; # remove commas and replace with tabs
-    print "\n$line\tno rank\tReadsreadsreads\t1\tconcat"; # print line with phylosift format
+    print "$line\tno rank\tReadsreadsreads\t1\tconcat\n"; # print line with phylosift format
 }
 
 exit;
