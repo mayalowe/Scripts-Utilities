049f279ea1bf3004fab232cc45c69e75b4ba9817
Eric Lowe <elowe@ucdavis.edu>
2012-09-19 15:48:18 -0700

    Lots of changes

--- phymmbl_to_ps.pl
+++ phymmbl_to_ps.pl
@@ -10,24 +10,28 @@ use IO::File;
 #use Net::FTP;
 #use Cwd;
 
-if (($#ARGV + 1) != 2) # if number of command-line arguments is incorrect print usage and exit
-{
+# if number of command-line arguments is incorrect print usage and exit
+if (($#ARGV + 1) != 2) {
     print "Wrong number of arguments!\n";
     print "usage: phymmbl_to_ps.pl [input] [output]\n";
     exit;
 }
 
-my $input = shift; # variable for input file
-my $output = shift; # variable for output file, to be replaced by return value of subroutine
-my $names = "/home/elowe/ncbi/names.dmp"; # path to nodes.dmp
+my $input        = shift; # variable for input file
+my $output       = shift; # variable for output file, to be replaced by return value of subroutine
+my $prefix       = '/home/elowe';
+my $names_file   = "$prefix/ncbi/names.dmp"; # path to nodes.dmp
+my $catalog_file = "$prefix/ncbi/RefSeq-release54.catalog";
+my $phymmbl_file = "$prefix/bin/phymmbl/.taxonomyData/.0_accessionMap/accessionMap.txt";
+my %taxed        = (); # hash with keys names and values taxids
 
-my %taxed = (); # hash with keys names and values taxids
-fill_names(\%taxed, $names);
+taxid_from_accession($catalog_file, $phymmbl_file);
+fill_names(\%taxed, $names_file);
 
-my %named;
-read_in(\%named, \%taxed, $input);
+#my %named;
+read_in(\%taxed, $input, $output);
 
-write_out($output); # calls subroutine to write out to file
+#write_out($output); # calls subroutine to write out to file
 
 exit;
 
@@ -39,59 +43,95 @@ exit;
 # from names.dmp to convert PhymmBL output to PS output.
 # Takes reference to %taxed and $names as parameters. 
 # Has no return value as of yet.
-sub fill_names
-{
-    my $ref = shift; # reference to %taxed
+sub fill_names {
+    my $taxed_ref = shift; # reference to %taxed
     my $file = shift;
 # Check here for how recent names.dmp is?
     # names.dmp filehandle
-    my $nfh = IO::File->new("< $file") or die "Could not open names.dmp for reading: $!"; 
+    my $names_fh = IO::File->new("< $file") 
+	or die "Could not open names.dmp for reading: $!"; 
 
-    while(<$nfh>)
-    {
+    while (<$names_fh>) {
 	chomp(my $line = $_);
 	$line =~ s/\|//g; # remove | from line
 	$line =~ s/ //g; # remove whitespace
 	my @field = split('\t', $line); # split on tabs to fill array with fields
 
-	if(! defined $$ref{$field[2]})
-	{
-	    $$ref{$field[2]} = $field[0];
+	if (! defined $$taxed_ref{$field[2]}) {
+	    $$taxed_ref{$field[2]} = $field[0];
 	}
     }
 
-    $nfh->close; # closes names.dmp filehandle
+    $names_fh->close; # closes names.dmp filehandle
 } # sub fill_names
 
 # sub read_in
 # Subroutine to read input PhymmBL file and extracts data from it appropriately.
 # Takes reference to %named, %taxed and $input as parameters.
 # Has no return value as of yet.
-sub read_in
-{
-    my $nref = shift; # reference to %named
-    my $tref = shift; # reference to %taxed
-    my $file = shift;
+sub read_in {
+#    my $named_ref = shift; # reference to %named
+    my $taxed_ref = shift; # reference to %taxed
+    my $input_file = shift;
+    my $output_file = shift;
     # input file filehandle
-    my $infh = IO::File->new("< $file") or die "Could not open $file for reading: $!"; 
+    my $infh = IO::File->new("< $input_file") 
+	or die "Could not open $input_file for reading: $!"; 
 
-    while(<$infh>)
-    {
+    while (<$infh>) {
 	next if $_ =~ 'QUERY_ID'; # skip first line of file
 	chomp(my $line = $_);
 	my @field = split('\t', $line);
-	print "$field[1], $field[0]\n";
+	if (! defined $$taxed_ref{$field[1]}) {
+	    print "Undefined name: $field[1]\n";
+	}
+	else {
+	    print "Defined name: $field[1]\n";
+	}
+#	print "$field[1], $field[0], $$taxed_ref{$field[1]}\n";
+#	my $hash_ref = { 
+#	    name => "$field[1]",
+#	    read_id => "$field[0]",
+#	    taxon_id => "$$taxed_ref{$field[1]}",
+#	}
     }
 
     $infh->close; # closes input file filehandle
 } # sub read_in
 
+
+# sub find_accession
+#
+sub find_accession {
+    my $organism_name = shift;
+
+    
+
+}
+# sub taxid_from_accession
+# Because PhymmBl doesn't play nice, finds taxid from
+# given mangled name and RefSeq Accession using PhymmBL
+# files and NCBI file
+sub taxid_from_accession {
+    my $catalog = shift;
+    my $phymmbl = shift;
+    my $phymmbl_fh = IO::File->new("< $phymmbl")
+	or die "Couldn't open $phymmbl for reading: $!";
+    my $catalog_fh = IO::File->new("< $catalog")
+	or die "Couldn't open $catalog for reading: $!";
+
+    
+
+    $phymmbl_fh->close;
+    $catalog_fh->close;
+
+} # sub taxid_from_accession
+
 # sub name_output
 # Subroutine to appropriately name output file based on name of input file.
 # Takes in name of input file as a parameter.
 # Return value is string containing name of output file.
-sub name_output
-{
+sub name_output {
     my $in = shift;
     my $name;
 
@@ -102,10 +142,11 @@ sub name_output
 # Subroutine to write out to a file in PS format.
 # Takes $output as the only parameter. 
 # Has no return value, closes output file at end of sub.
-sub write_out
-{
+sub write_out {
+    my $line_ref = shift;
     my $file = shift; # local variable for output file
-    my $ofh = IO::File->new("> $file") or die "Could not open $file for writing: $!";
+    my $ofh = IO::File->new(">> $file") 
+	or die "Could not open $file for writing: $!";
 
     $ofh->close;
 } # sub write_out
