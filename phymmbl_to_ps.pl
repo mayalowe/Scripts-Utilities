e25eeca8850a97cff9b57294e31571848b760ba0
Eric Lowe <elowe@ucdavis.edu>
2012-09-19 18:27:00 -0700

    Changes changes

--- phymmbl_to_ps.pl
+++ phymmbl_to_ps.pl
@@ -11,25 +11,34 @@ use IO::File;
 #use Cwd;
 
 # if number of command-line arguments is incorrect print usage and exit
-if (($#ARGV + 1) != 2) {
+if (($#ARGV + 1) < 2) {
     print "Wrong number of arguments!\n";
     print "usage: phymmbl_to_ps.pl [input] [output]\n";
     exit;
 }
+my $prefix1 = q(); # initialize prefix variables to empty strings
+my $prefix2 = q();
+my $input   = shift; # variable for input file
+my $output  = shift; # variable for output file, to be replaced by return value of subroutine
 
-my $input        = shift; # variable for input file
-my $output       = shift; # variable for output file, to be replaced by return value of subroutine
-my $prefix       = '/home/elowe';
-my $names_file   = "$prefix/ncbi/names.dmp"; # path to nodes.dmp
-my $catalog_file = "$prefix/ncbi/RefSeq-release54.catalog";
-my $phymmbl_file = "$prefix/bin/phymmbl/.taxonomyData/.0_accessionMap/accessionMap.txt";
-my %taxed        = (); # hash with keys names and values taxids
+# if optional third argument given
+if ($ARGV[2]) {
+    $prefix1 = $ARGV[2];
+    $prefix2 = $prefix1;
+}
+else {
+    $prefix1 = '/home/elowe/ncbi';
+    $prefix2 = '/home/elowe/bin/phymmbl/.taxonomyData/.0_accessionMap';
+}
 
-taxid_from_accession($catalog_file, $phymmbl_file);
-fill_names(\%taxed, $names_file);
+my $catalog_file = "$prefix1/RefSeq-release54.catalog";
+my $phymmbl_file = "$prefix2/accessionMap.txt";
 
-#my %named;
-read_in(\%taxed, $input, $output);
+my $ncbi_org_ref = get_ncbi_accessions($catalog_file);
+my $phymmbl_org_ref = get_phymmbl_accessions($phymmbl_file);
+
+
+read_in($ncbi_org_ref, $phymmbl_org_ref, $input, $output);
 
 #write_out($output); # calls subroutine to write out to file
 
@@ -38,56 +47,77 @@ exit;
 ######################SUBROUTINES##########################
 ###########################################################
 
-# sub fill_names
-# Subroutine to populate hash with names and taxids
-# from names.dmp to convert PhymmBL output to PS output.
-# Takes reference to %taxed and $names as parameters. 
-# Has no return value as of yet.
-sub fill_names {
-    my $taxed_ref = shift; # reference to %taxed
+
+
+sub get_ncbi_accessions {
     my $file = shift;
-# Check here for how recent names.dmp is?
-    # names.dmp filehandle
-    my $names_fh = IO::File->new("< $file") 
-	or die "Could not open names.dmp for reading: $!"; 
-
-    while (<$names_fh>) {
-	chomp(my $line = $_);
-	$line =~ s/\|//g; # remove | from line
-	$line =~ s/ //g; # remove whitespace
-	my @field = split('\t', $line); # split on tabs to fill array with fields
-
-	if (! defined $$taxed_ref{$field[2]}) {
-	    $$taxed_ref{$field[2]} = $field[0];
-	}
+    my $catalog_fh = IO::File->new("< $file")
+        or die "Couldn't open $file for reading: $!";
+        
+    my %ncbi_organism;
+    while (<$catalog_fh>) {
+        chomp(my $line = $_);
+        $line =~ s/ //g;
+        my @fields = split('\t', $line);
+        $fields[2] =~ s/\.\d//g;
+        
+        if (! defined $ncbi_organism{ $fields[2] }) {
+            $ncbi_organism{ $fields[2] } = [ $fields[1], $fields[0] ];
+        }
+        else {
+            print "We've seen $fields[2] before!\n";
+        }
     }
+    return \%ncbi_organism;
+    
+} # sub get_ncbi_accessions
 
-    $names_fh->close; # closes names.dmp filehandle
-} # sub fill_names
+sub get_phymmbl_accessions {
+    my $file = shift;
+    my $phymmbl_fh = IO::File->new("< $file")
+        or die "Couldn't open $file for reading: $!";
+        
+    my %phymmbl_organism;
+    
+    while (<$phymmbl_fh>) {
+        chomp(my $line = $_);
+        $line =~ s/ //g;
+        my @fields = split('\t', $line);
+        
+        if (! defined $phymmbl_organism{ $fields[0] }) {
+            $phymmbl_organism{ $fields[0] } = [ $fields[1] ];
+        }
+        else {
+            push @{ $phymmbl_organism{$fields[0]} }, $fields[1];
+        }
+    }
+    return \%phymmbl_organism;
+} # sub get_phymmbl_accessions
 
 # sub read_in
 # Subroutine to read input PhymmBL file and extracts data from it appropriately.
 # Takes reference to %named, %taxed and $input as parameters.
 # Has no return value as of yet.
 sub read_in {
-#    my $named_ref = shift; # reference to %named
-    my $taxed_ref = shift; # reference to %taxed
+    my $ncbi_ref = shift; # reference to %named
+    my $phymmbl_ref = shift; # reference to %taxed
     my $input_file = shift;
     my $output_file = shift;
+    
     # input file filehandle
     my $infh = IO::File->new("< $input_file") 
-	or die "Could not open $input_file for reading: $!"; 
+	   or die "Could not open $input_file for reading: $!"; 
 
     while (<$infh>) {
-	next if $_ =~ 'QUERY_ID'; # skip first line of file
-	chomp(my $line = $_);
-	my @field = split('\t', $line);
-	if (! defined $$taxed_ref{$field[1]}) {
-	    print "Undefined name: $field[1]\n";
-	}
-	else {
-	    print "Defined name: $field[1]\n";
-	}
+	   next if $_ =~ 'QUERY_ID'; # skip first line of file
+	   chomp(my $line = $_);
+	   my @field = split('\t', $line);
+	   if (! defined $$phymmbl_ref{$field[1]}) {
+	       print "Undefined name: $field[1]\n";
+	   }
+	   else {
+	       print "Defined name: $field[1]\n";
+	   }
 #	print "$field[1], $field[0], $$taxed_ref{$field[1]}\n";
 #	my $hash_ref = { 
 #	    name => "$field[1]",
@@ -99,33 +129,6 @@ sub read_in {
     $infh->close; # closes input file filehandle
 } # sub read_in
 
-
-# sub find_accession
-#
-sub find_accession {
-    my $organism_name = shift;
-
-    
-
-}
-# sub taxid_from_accession
-# Because PhymmBl doesn't play nice, finds taxid from
-# given mangled name and RefSeq Accession using PhymmBL
-# files and NCBI file
-sub taxid_from_accession {
-    my $catalog = shift;
-    my $phymmbl = shift;
-    my $phymmbl_fh = IO::File->new("< $phymmbl")
-	or die "Couldn't open $phymmbl for reading: $!";
-    my $catalog_fh = IO::File->new("< $catalog")
-	or die "Couldn't open $catalog for reading: $!";
-
-    
-
-    $phymmbl_fh->close;
-    $catalog_fh->close;
-
-} # sub taxid_from_accession
 
 # sub name_output
 # Subroutine to appropriately name output file based on name of input file.
