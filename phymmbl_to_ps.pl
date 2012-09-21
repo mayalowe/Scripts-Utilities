ec4adb262aa6f30d38ff97d7992b4011659d8320
Eric Lowe <elowe@ucdavis.edu>
2012-09-21 13:42:01 -0700

    Updated scripts

--- phymmbl_to_ps.pl
+++ phymmbl_to_ps.pl
@@ -11,27 +11,17 @@ use IO::File;
 #use Cwd;
 
 # if number of command-line arguments is incorrect print usage and exit
-if (($#ARGV + 1) < 2) {
+if (($#ARGV + 1) != 1) {
     print "Wrong number of arguments!\n";
-    print "usage: phymmbl_to_ps.pl [input] [output]\n";
+    print "usage: phymmbl_to_ps.pl [input]\n";
     exit;
 }
-my $prefix1 = q(); # initialize prefix variables to empty strings
-my $prefix2 = q();
+my $prefix1 = '/home/elowe/ncbi'; # initialize prefix variables
+my $prefix2 = '/home/elowe/bin/phymmbl/.taxonomyData/.0_accessionMap';
 my $input   = shift; # variable for input file
-my $output  = shift; # variable for output file, to be replaced by return value of subroutine
+my $output  = name_output($input); # variable for output file, to be replaced by return value of subroutine
 
-# if optional third argument given
-if ($ARGV[2]) {
-    $prefix1 = $ARGV[2];
-    $prefix2 = $prefix1;
-}
-else {
-    $prefix1 = '/home/elowe/ncbi';
-    $prefix2 = '/home/elowe/bin/phymmbl/.taxonomyData/.0_accessionMap';
-}
-
-my $catalog_file = "$prefix1/RefSeq-release54.catalog";
+my $catalog_file = "$prefix1/RefSeq-release55.catalog";
 my $phymmbl_file = "$prefix2/accessionMap.txt";
 my $ncbi_org_ref = get_ncbi_accessions($catalog_file);
 my $phymmbl_org_ref = get_phymmbl_accessions($phymmbl_file);
@@ -43,8 +33,13 @@ exit;
 ######################SUBROUTINES##########################
 ###########################################################
 
-
-
+# sub get_ncbi_accessions
+# Subroutine to fill hash from NCBI RefSeq catalog file with keys
+# of NCBI Accession numbers and a value of an array with elements 
+# of the organism name and the organism taxonomic id.  This allows 
+# for conversion of PhymmBL output to Phylosift output. Only parameter
+# is the location of the NCBI catalog file.  Will later be extended
+# to check for newer version of catalog file. Return value is a hash reference.
 sub get_ncbi_accessions {
     my $file = shift;
     my $catalog_fh = IO::File->new("< $file")
@@ -53,21 +48,32 @@ sub get_ncbi_accessions {
     my %ncbi_organism;
     while (<$catalog_fh>) {
         chomp(my $line = $_);
-        $line =~ s/ //g;
-        my @fields = split('\t', $line);
-        $fields[2] =~ s/\.\d//g;
-        
+
+        my @fields = split('\t', $line); # split fields on tabs
+	# remove trailing period and number on NCBI accession so accession
+	# can be used as key with phymmbl_organism hash
+        $fields[2] =~ s/\.\d+//g; 
+        # if accession not yet defined, assign name and taxid to elements in array
         if (! defined $ncbi_organism{ $fields[2] }) {
-            $ncbi_organism{ $fields[2] } = [ $fields[1], $fields[0] ];
+            $ncbi_organism{ $fields[2] }->[0] = $fields[1]; # organism name
+	    $ncbi_organism{ $fields[2] }->[1] = $fields[0]; # organism taxid
         }
         else {
             print "We've seen $fields[2] before!\n";
         }
     }
     return \%ncbi_organism;
-    
 } # sub get_ncbi_accessions
 
+# sub get_phymmbl_accessions
+# Subroutine that opens the PhymmBL accession map file and fills a hash
+# with keys of organism names and values of associated RefSeq accession 
+# numbers.  Since any one organism can have a number of possible accession
+# numbers, this subroutine keeps only the first as that accession should 
+# be present in the NCBI RefSeq catalog file.  This allows the script to 
+# find the taxid and name for organisms reported by PhymmBL for accurate
+# output conversion. Parameter is the location of the phymmbl Accession map
+# file.  Return value is a hash reference. 
 sub get_phymmbl_accessions {
     my $file = shift;
     my $phymmbl_fh = IO::File->new("< $file")
@@ -77,15 +83,12 @@ sub get_phymmbl_accessions {
     
     while (<$phymmbl_fh>) {
         chomp(my $line = $_);
-        $line =~ s/ //g;
-        my @fields = split('\t', $line);
-        
+        $line =~ s/ //g; # remove whitespace
+        my @fields = split('\t', $line); # split fields on tab
+        # if organism name is not yet defined, define with accession
         if (! defined $phymmbl_organism{ $fields[0] }) {
-            $phymmbl_organism{ $fields[0] } = [ $fields[1] ];
-        }
-        else {
-            push @{ $phymmbl_organism{$fields[0]} }, $fields[1];
-        }
+            $phymmbl_organism{ $fields[0] } = $fields[1];
+	}
     }
     return \%phymmbl_organism;
 } # sub get_phymmbl_accessions
@@ -118,27 +121,38 @@ sub read_in {
 	   else {
 	       #print "Defined name: $field[1]\n";
 	       my $accession = $$phymmbl_ref{$field[1]};
-	       $name = $ncbi_ref->{$accession}[0];
-           $read_id = $field[0];
-           $taxon_id = $ncbi_ref->{$accession}[1];
-           write_out($name, $read_id, $taxon_id, $output_file);
+	       if (! defined $$ncbi_ref{$accession}) {
+		   print "$field[1]\n";
+		   print "$accession\n";
+	       }
+	       else {
+		   $name = $ncbi_ref->{$accession}[0];
+		   $read_id = $field[0];
+		   $taxon_id = $ncbi_ref->{$accession}[1];
+		   write_out($name, $read_id, $taxon_id, $output_file);
+	       }
 	   }
-#	print "$field[1], $field[0], $$taxed_ref{$field[1]}\n";
     }
     $infh->close; # closes input file filehandle
 } # sub read_in
-
 
 # sub name_output
 # Subroutine to appropriately name output file based on name of input file.
 # Takes in name of input file as a parameter.
 # Return value is string containing name of output file.
 sub name_output {
-    my $in = shift;
-    my $name;
-
-    return($name);
-}
+    my $input = shift;
+    my $name = '/home/elowe/PS_temp/';
+    
+    if ($input =~ 'knockoutunif') {
+	$name .= 'knockoutunif_ill_fastq-reads_phymmbl.fastq/sequence_taxa.txt';
+    }
+    elsif ($input =~ 'knockoutexp') {
+	$name .= 'knockoutexp_ill_fastq-reads_phymmbl.fastq/sequence_taxa.txt';
+    }
+    print "Output file name is $name\n";
+    return $name;
+} # sub name_output
 
 # sub write_out 
 # Subroutine to write out to a file in PS format.
@@ -152,7 +166,7 @@ sub write_out {
     my $ofh = IO::File->new(">> $file") 
 	   or die "Could not open $file for writing: $!";
 	   
-	print $ofh "$read_id\t$taxon_id\tno rank\t$name\t1\tconcat\n";
+    print $ofh "$read_id\t$taxon_id\tno rank\t$name\t1\tconcat\n";
 
     $ofh->close;
 } # sub write_out
